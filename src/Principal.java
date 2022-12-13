1a33c2165627faa413030710a14b4c1f335eebf4
javierhol <javierholguin596@gmail.com>
2022-12-12 21:35:26 -0500

    Project with UI

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,13 @@
-public class 
-        Principal {
-    public static void main(String[] args) {
+import javax.swing.*;
 
-        new Procesos();
+public class Principal {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ventanita");
+        frame.setContentPane(new Ventanita().miPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(500,500);
     }
 }
--- src/Procesos.java
+++ src/Procesos.java
@@ -1,54 +1,16 @@
-import javax.swing.*;
-import java.util.ArrayList;
 import java.util.HashMap;
-
 public class Procesos {
-    double n1,n2,n3;
+ Ventanita miVentanita = new Ventanita();
     int cantGanan=0, cantpierden=0, cantRecupera=0, cantPierden=0 ,cantEstudiantesValidados=0;
-    String nombre;
-
-   // HashMap<String ,ArrayList<Double>> lista = new HashMap<String, ArrayList<Double>>();
-
     HashMap<String,Double> notasEstudent = new HashMap<String,Double>();
-    public Procesos() {
-        pedirNotas();
-    }
-
-
-
-    private void pedirNotas() {
-        int cant = Integer.parseInt(JOptionPane.showInputDialog("Cuantos estudiantes desea ingresar?"));
-
-        for(int i=0;i<cant;i++){
-            nombre = JOptionPane.showInputDialog("Ingrese el nombre del estudiante");
-
-
-            do {
-                n1 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota 1"));
-                if (n1<0 || n1>5) {
-                    JOptionPane.showMessageDialog(null, "La nota 1 debe estar entre 0 y 5");
-                }
-            }while (n1<0 || n1>5);
-           do {
-               n2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota 2"));
-               if (n2<0 || n2>5) {
-                   JOptionPane.showMessageDialog(null, "La nota 2 debe estar entre 0 y 5");
-               }
-           }while (n2<0 || n2>5);
-
-           do {
-               n3 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la nota 3"));
-               if (n3<0 || n3>5) {
-                   JOptionPane.showMessageDialog(null, "La nota 3 debe estar entre 0 y 5");
-               }
-           }while (n3<0 || n3>5);
+    public void pedirNotas(double n1, double n2, double n3, String nombre) {
 
             double promedio = (n1+n2+n3)/3;
             if (promedio>=3.5) {
-                JOptionPane.showMessageDialog(null, "El estudiante "+nombre+" aprobo con un promedio de "+promedio);
+               miVentanita.result1.setText( "El estudiante "+nombre+" aprobo con un promedio de "+promedio);
                 cantGanan++;
             }else{
-                JOptionPane.showMessageDialog(null, "El estudiante "+nombre+" reprobo con un promedio de "+promedio);
+                miVentanita.result1.setText( "El estudiante "+nombre+" reprobo con un promedio de "+promedio);
                 cantpierden++;
             }
             if (promedio > 2.0 && promedio < 3.5) {
@@ -58,18 +20,11 @@ public class Procesos {
             }
             notasEstudent.put(nombre,promedio);
             cantEstudiantesValidados++;
-        }
-        imprimirDatos();
-        System.out.println("-------------------------------------------------");
-        promedioTotal();
-        System.out.println("-------------------------------------------------");
-        imprimirLitado();
     }
-
     public void mostrarEstudiantes(){
         System.out.println("Lista de estudiantes :");
         for (String key : notasEstudent.keySet()) {
-            System.out.println("Nombre: "+key + " | "+ "Promedio: " + notasEstudent.get(key));
+            miVentanita.result2.setText("Nombre: "+key + " | "+ "Promedio: " + notasEstudent.get(key));
         }
     }
     public void promedioTotal(){
@@ -77,23 +32,16 @@ public class Procesos {
         for (String key : notasEstudent.keySet()) {
             promedioTotal += notasEstudent.get(key);
         }
-        System.out.println("El promedio de las notas finales obtenidas es : "+promedioTotal/notasEstudent.size());
-
+        miVentanita.result3.setText("El promedio de las notas finales obtenidas es : "+promedioTotal/notasEstudent.size());
     }
-
-
     public void imprimirDatos(){
-
-        System.out.println("Cantidad de estudiantes validados : "+cantEstudiantesValidados);
-        System.out.println("Cantidad de notas ingresadas es : "+cantEstudiantesValidados*3);
-        System.out.println("Cantidad de estudiantes que ganaron: "+cantGanan);
-        System.out.println("Cantidad de estudiantes que perdieron: "+cantpierden);
-        System.out.println("Cantidad de estudiantes que pueden recuperar: "+cantRecupera);
-        System.out.println("Cantidad de estudiantes que perdieron sin recuperación: "+cantPierden);
-    }
-
-    public void imprimirLitado(){
+        miVentanita.result4.setText("Cantidad de estudiantes validados : "+cantEstudiantesValidados);
+        miVentanita.result5.setText("Cantidad de notas ingresadas es : "+cantEstudiantesValidados*3);
+        miVentanita.result6.setText("Cantidad de estudiantes que ganaron: "+cantGanan);
+        miVentanita.result7.setText("Cantidad de estudiantes que perdieron: "+cantpierden);
+        miVentanita.result8.setText("Cantidad de estudiantes que pueden recuperar: "+cantRecupera);
+        miVentanita.result9.setText("Cantidad de estudiantes que perdieron sin recuperación: "+cantPierden);
+        promedioTotal();
         mostrarEstudiantes();
     }
-
 }
